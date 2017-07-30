d403409383ca92b0bfa14a04842777d99ab1f272
RezzedUp <RezzedUp@users.noreply.github.com>
2017-07-30 15:42:20 -0500

    Actually *include* the refactoring this time.

--- src/main/java/com/rezzedup/opguard/Password.java
+++ src/main/java/com/rezzedup/opguard/Password.java
@@ -21,37 +21,91 @@ public final class Password
     }
     
     @Deprecated
+    public static final Algorithm DEPRECATED = new Algorithm() 
     {
-    
+        @Override
+        public String generateSalt()
         {
-        
-            for (byte b : hash)
+            return DEPRECATED_GLOBAL_SALT;
+        }
+    
+        @Override
+        public String hash(String plaintext, String salt)
+        {
+            String pass = plaintext + salt;
+    
+            try
             {
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(pass.getBytes("UTF-8"));
+                StringBuilder hashed = new StringBuilder();
+        
+                for (byte b : hash)
+                {
+                    hashed.append(String.format("%02X", b));
+                }
+        
+                return hashed.toString().toLowerCase();
+            }
+            catch (NoSuchAlgorithmException | UnsupportedEncodingException e)
+            {
+                throw new RuntimeException(e);
             }
         }
+    
+        @Override
+        public boolean check(String plaintext, String hash)
         {
+            return hash(plaintext, DEPRECATED_GLOBAL_SALT).equalsIgnoreCase(hash);
         }
     };
     
+    public static final Algorithm BCRYPT = new Algorithm() 
+    {
+        @Override
+        public String generateSalt()
+        {
+            return BCrypt.gensalt();
+        }
+    
+        @Override
+        public String hash(String plaintext, String salt)
+        {
+            return BCrypt.hashpw(plaintext, salt);
+        }
+    
+        @Override
+        public boolean check(String plaintext, String hash)
+        {
+            return BCrypt.checkpw(plaintext, hash);
+        }
+    };
     
     public static Password fromDeprecated(String hashedSource)
     {
+        return new Password(DEPRECATED, hashedSource);
     }
     
+    public static Password from(String hashedSource)
     {
+        return new Password(BCRYPT, hashedSource);
     }
     
     public static Password create(String plaintext)
     {
+        return new Password(BCRYPT, BCRYPT.hash(plaintext, BCRYPT.generateSalt()));
     }
     
+    private final Algorithm algorithm;
     private final String hash;
     
+    private Password(Algorithm algorithm, String hash)
     {
         this.algorithm = algorithm;
         this.hash = hash;
     }
     
+    public Algorithm getAlgorithm()
     {
         return this.algorithm;
     }
@@ -63,9 +117,11 @@ public final class Password
     
     public boolean isDeprecated()
     {
+        return this.algorithm == DEPRECATED;
     }
     
     public boolean check(String plaintext)
     {
+        return algorithm.check(plaintext, hash);
     }
 }
